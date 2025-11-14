20ad36640564d10235ba3efc513dd8f349eaa873
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2025-11-14 17:15:53 +0100

    feat(scene-manager): add SceneLoader to handle scene loading when the game starts

---
+++ EchoCity-Game/Assets/Scripts/Managers/SceneManager/SceneLoader.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private string firstLevelName = "Noah's Lab";
+    [SerializeField] private string debugSceneName = "none";
+    private string _currentLevelName;
+    private void Start()
+    {
+        StartCoroutine(LoadLevelAdditive(firstLevelName));
+        if (debugSceneName != "none") StartCoroutine(LoadSceneAdditiveNoActive(debugSceneName));
+    }
+    public IEnumerator LoadLevelAdditive(string sceneName)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        op.allowSceneActivation = true;
+
+        while (!op.isDone)
+        {
+            //NOTE Loading bar here
+            yield return null;
+        }
+
+        _currentLevelName = sceneName;
+        Scene levelScene = SceneManager.GetSceneByName(sceneName);
+        if (levelScene.IsValid())
+        {
+            SceneManager.SetActiveScene(levelScene);
+        }
+
+        PlacePlayerOnSpawn();
+        Log.D("Loaded active scene: " + sceneName, "red", "SceneLoader");
+    }
+
+    public void PlacePlayerOnSpawn()
+    {
+        GameObject spawn = GameObject.FindWithTag("Respawn");
+        GameObject player = GameObject.FindWithTag("Player");
+
+        if (spawn != null && player != null)
+        {
+            player.transform.position = spawn.transform.position;
+            player.transform.rotation = spawn.transform.rotation;
+        }
+
+    }
+
+    public IEnumerator LoadSceneAdditiveNoActive(string sceneName)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        while (!op.isDone)
+        {
+            yield return null;
+        }
+
+        Log.D("Loaded non-active scene: " + sceneName, "red", "SceneLoader");
+    }
+
+
+
+
+
+
+}
