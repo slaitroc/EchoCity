843906d07f4b3c744736a0488f19a2939554a526
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2025-11-15 17:34:23 +0100

    refactor(events): rename handler methods with "Handler" suffix

--- EchoCity-Game/Assets/Scripts/Events/GameManager_EventTest.cs
+++ EchoCity-Game/Assets/Scripts/Events/GameManager_EventTest.cs
@@ -15,24 +15,24 @@ public class GameManager_EventTest : MonoBehaviour
         pause = false;
     }
 
-    public void PauseGame()
+    public void PauseGameHandler()
     {
         if (!pause) gameStateFSM.ChangeState(GameStatesEnum.PAUSE);
         else gameStateFSM.ChangeState(GameStatesEnum.PLAYING);
         pause = !pause;
     }
 
-    public void WriteMessage()
+    public void WriteMessageHandler()
     {
         Log.D("Void event triggered by pressing 'E'", "green", "GAME MANAGER"); // NOTE: 'E' is hardcoded
     }
 
-    public void WriteMessage(string message)
+    public void WriteMessageHandler(string message)
     {
         Log.D(message, "green", "GAME MANAGER");
     }
 
-    public void WriteIntMessage(int value)
+    public void WriteMessageHandler(int value)
     {
         Log.D("Int event raised with value: " + value, "green", "GAME MANAGER");
     }
