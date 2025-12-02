de5253c62fc28a9f5639d328480dfa85be92e7ba
Samuel Runmark Thunell <samuel.runmark@gmail.com>
2025-12-02 23:26:52 +0100

    feat(enemy): animation-based footsteps
    - set keyframes in the enemies walk animation sends
      audiosphere and audio events
    - uses player footsteps right now but should be heavy-sounding later on

---
+++ EchoCity-Game/Assets/Scripts/Enemies/EnemyFootsteps.cs
@@ -0,0 +1,39 @@
+using EchoCity;
+using UnityEngine;
+
+public class EnemyFootsteps : MonoBehaviour
+{
+    [System.Serializable]
+    public class GroundFootstep
+    {
+        public LayerMask layer;
+        public SOSoundSource SoundSource;
+    }
+
+    [Header("Invoking Events")]
+    public SOSoundEmissionDataEvent newAudioSphereEvent;
+
+    [Header("Footstep Settings")]
+    public GroundFootstep[] groundTypes;
+    public float rayDistance = 1.5f;
+
+    private void PlayFootstepSound()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, rayDistance))
+        {
+            int hitLayer = hit.collider.gameObject.layer;
+
+            for (int i = 0; i < groundTypes.Length; i++)
+            {
+                if ((groundTypes[i].layer.value & (1 << hitLayer)) != 0)
+                {
+                    SOSoundSource soundSource = groundTypes[i].SoundSource;
+                    ECSound.PlayRandomClipAtPosition(soundSource, hit.point, newAudioSphereEvent, "SFX");
+                    return;
+                }
+            }
+        }
+    }
+}
