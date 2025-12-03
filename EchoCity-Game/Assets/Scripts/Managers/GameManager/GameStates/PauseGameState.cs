a4282eb7e571cde450d780a0a9c402df592640a7
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2025-12-04 00:51:24 +0100

    fix(pause): simplify PauseGameState by removing unused player input handling

--- EchoCity-Game/Assets/Scripts/Managers/GameManager/GameStates/PauseGameState.cs
+++ EchoCity-Game/Assets/Scripts/Managers/GameManager/GameStates/PauseGameState.cs
@@ -8,18 +8,11 @@ public class PauseGameState : GameState
 {
     [Header("UI Action Map")]
     private const string UI_ACTION_MAP = "UI";
-    private PlayerInput _playerInput;
-    private string _previousActionMap;
 
-    public PauseGameState(GameManager gameManager, GameStatesFSM fsm) : base(gameManager, fsm)
-    {
-    }
-
+    public PauseGameState(GameManager gameManager, GameStatesFSM fsm) : base(gameManager, fsm) { }
     public override void Enter()
     {
         Time.timeScale = 0;
-        _playerInput = GameObject.FindGameObjectWithTag("InputManager").GetComponent<PlayerInput>();
-        // SwitchToUIActionMap();
     }
 
     public override void Update()
@@ -29,22 +22,9 @@ public class PauseGameState : GameState
     public override void Exit()
     {
         Time.timeScale = 1;
-        // RestorePreviousActionMap();
     }
 
-    private void SwitchToUIActionMap()
-    {
-        if (_playerInput != null)
-        {
-            _previousActionMap = _playerInput.currentActionMap.name;
-            _playerInput.SwitchCurrentActionMap(UI_ACTION_MAP);
-        }
-    }
 
-    private void RestorePreviousActionMap()
-    {
-        if (_playerInput != null) _playerInput.SwitchCurrentActionMap(_previousActionMap);
-    }
 
     public override GameStatesEnum GetEnum()
     {
