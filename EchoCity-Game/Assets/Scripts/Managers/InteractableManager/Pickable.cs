c2866ab64eed8d75c369857e8dd12563ce50170c
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2025-12-07 18:45:15 +0100

    feat(pickable): refactor access modifiers and remove unused methods

--- EchoCity-Game/Assets/Scripts/Managers/InteractableManager/Pickable.cs
+++ EchoCity-Game/Assets/Scripts/Managers/InteractableManager/Pickable.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 namespace EchoCity.Interactables
 {
     [RequireComponent(typeof(Collider))]
-    public class Pickable : Interactable
+    public abstract class Pickable : Interactable
     {
         protected override string _TYPE_LOG_TAG => "PICKABLE";
         protected override string _LOG_TAG => pickableData != null ? pickableData.name : "NO_DATA";
 
         [Header("Invoking Events")]
-        [SerializeField] private SOPickableDataGameObjectEvent itemPickedEvent;
-        [SerializeField] private SOSoundEmissionDataEvent newAudioSphereEvent;
+        [SerializeField] protected SOPickableDataGameObjectEvent itemPickedEvent;
+        [SerializeField] protected SOSoundEmissionDataEvent newAudioSphereEvent;
         [Header("Observing Events")]
-        [SerializeField] private SOBoolEvent canBePickedEvent;
+        [SerializeField] protected SOBoolEvent canBePickedEvent;
         [Header("Pickable Data")]
-        [SerializeField] private SOPickable pickableData;
+        [SerializeField] protected SOPickable pickableData;
 
-        private bool _canBePicked = false;
+        protected bool _canBePicked = false;
 
 
         public override void Interact()
@@ -51,21 +51,5 @@ namespace EchoCity.Interactables
             if (canBePickedEvent)
                 canBePickedEvent.OnEventRaised -= InventoryHandler;
         }
-
-        public override void CheckTags(PuzzleTagEnum[] tagsToCheck)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void SetTags(PuzzleTagEnum[] tagsToSet)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override void InteractionOutcomeHandler(bool outcome)
-        {
-        }
-
-
     }
 }
