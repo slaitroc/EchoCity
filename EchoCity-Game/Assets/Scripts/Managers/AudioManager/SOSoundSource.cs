f57a91a29183dacadd336ba3d1e8272298213127
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2025-12-21 18:06:16 +0100

    feat: enhance SOSoundClass and SOSoundSource with additional properties and validation for audio length

--- EchoCity-Game/Assets/Scripts/Managers/AudioManager/SOSoundSource.cs
+++ EchoCity-Game/Assets/Scripts/Managers/AudioManager/SOSoundSource.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class SOSoundSource : ScriptableObject
 {
     [SerializeField] private SOSoundClass soundClass;
+    [SerializeField] private float soundClassIntensityFactorMultiplier = 1f;
     #region Serialized Fields
     [Header("Echo Settings")]
     [SerializeField] private AudioClip audioClip;
+    [SerializeField] private float audioLengthReference ;
+    [SerializeField] private float audioLengthOverride = 0f;
     [Range(0f, 1f)][SerializeField] private float intensity;
     [Range(0f, 1f)][SerializeField] private float volume;
     [Range(0f, 50f)][SerializeField, Min(0f)] private float radius;
@@ -23,7 +26,9 @@ public class SOSoundSource : ScriptableObject
 
     #region Public Properties
     public SOSoundClass SoundClass => soundClass;
+    public float SoundClassIntensityFactorMultiplier => soundClassIntensityFactorMultiplier;
     public AudioClip AudioClip => audioClip;
+    public float AudioLengthOverride => audioLengthOverride;
     public float Intensity => intensity;
     public float Volume => volume;
     public float Radius => radius;
@@ -33,5 +38,24 @@ public class SOSoundSource : ScriptableObject
     public AudioClip[] RandomAudioClips => randomAudioClips;
     #endregion
 
+    void OnValidate()
+    {
+        if (randomAudioClips != null && randomAudioClips.Length > 0)
+        {
+            float maxLength = 0f;
+            foreach (var clip in randomAudioClips)
+            {
+                if (clip != null && clip.length > maxLength)
+                {
+                    maxLength = clip.length;
+                }
+            }
+            audioLengthReference = Mathf.Max(audioLengthReference, maxLength);
+        }
+        else if (audioClip != null)
+        {
+            audioLengthReference = audioClip.length;
+        }
+    }
 
 }
