4d3db868f9342f9a8c60663f199fba1638299299
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2025-12-21 18:24:01 +0100

    chore: remove SOEnemyInvestigationEvent script and associated meta file

--- EchoCity-Game/Assets/Scripts/Events/Blueprints/SOEnemyInvestigationEvent.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace EchoCity
-{
-    [CreateAssetMenu(fileName = "EnemyInvestigationEventSO", menuName = "ECHO CITY/ENEMY/EnemyInvestigationEventSO")]
-    public class SOEnemyInvestigationEvent : SOEvent<EnemyInvestigationData> { }
-}
