9484970579131bb35d01994fdeb02a02d700800f
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2025-12-22 18:18:26 +0100

    feat(scene): reorder SceneEnum values for consistency with scene loader

--- EchoCity-Game/Assets/Scripts/Events/Blueprints/SOScenesEnumEvent.cs
+++ EchoCity-Game/Assets/Scripts/Events/Blueprints/SOScenesEnumEvent.cs
@@ -5,11 +5,12 @@ namespace EchoCity
     [System.Serializable]
     public enum SceneEnum
     {
+        //NOTE: numbers must match scene loader's scenes array indices
         None = 0,
         Persistent = 1,
-        Level1 = 2,
-        Level2 = 3,
-        Playground = 100
+        Playground = 2,
+        Level1 = 3,
+        Level2 = 4,
     }
 
     [CreateAssetMenu(fileName = "SceneEnumEventSO", menuName = "ECHO CITY/Events/SceneEnumEventSO")]
