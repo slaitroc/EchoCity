1c9852f40ebc7dc87a82187a09e4bf0bf3f863f5
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2025-12-29 23:16:11 +0100

    refactor: update log methods in MaterialSwitcher

--- EchoCity-Game/Assets/Scripts/Managers/EcholocationManager/MaterialSwitcher.cs
+++ EchoCity-Game/Assets/Scripts/Managers/EcholocationManager/MaterialSwitcher.cs
@@ -1,66 +1,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class MaterialSwitcher
+namespace EchoCity
 {
-    private static Dictionary<Renderer, Material[]> _originalMaterials;
-    private static bool _hasStoredMaterials;
-
-    public static void StoreOriginalMaterials()
+    public static class MaterialSwitcher
     {
-        _originalMaterials = new Dictionary<Renderer, Material[]>();
+        private static Dictionary<Renderer, Material[]> _originalMaterials;
+        private static bool _hasStoredMaterials;
 
-        var renderers = Object.FindObjectsByType<Renderer>(
-            FindObjectsInactive.Include,
-            FindObjectsSortMode.None
-        );
-        foreach (var r in renderers)
+        public static void StoreOriginalMaterials()
         {
-            if (r == null) continue;
-            _originalMaterials[r] = r.sharedMaterials;
+            _originalMaterials = new Dictionary<Renderer, Material[]>();
+
+            var renderers = Object.FindObjectsByType<Renderer>(
+                FindObjectsInactive.Include,
+                FindObjectsSortMode.None
+            );
+            foreach (var r in renderers)
+            {
+                if (r == null) continue;
+                _originalMaterials[r] = r.sharedMaterials;
+            }
+
+            _hasStoredMaterials = true;
         }
 
-        _hasStoredMaterials = true;
-    }
-
-    public static void ApplyOverrideMaterial(Material overrideMaterial)
-    {
-        if (!_hasStoredMaterials)
-            StoreOriginalMaterials();
-
-        if (overrideMaterial == null)
+        public static void ApplyOverrideMaterial(Material overrideMaterial)
         {
-            Log.E("MaterialUtils.ApplyOverrideMaterial: overrideMaterial is NULL!");
-            return;
+            if (!_hasStoredMaterials)
+                StoreOriginalMaterials();
+
+            if (overrideMaterial == null)
+            {
+                Log.ELazy(() => "MaterialUtils.ApplyOverrideMaterial: overrideMaterial is NULL!");
+                return;
+            }
+
+            foreach (var kvp in _originalMaterials)
+            {
+                var r = kvp.Key;
+                if (r == null) continue;
+
+                var newMats = new Material[r.sharedMaterials.Length];
+                for (var i = 0; i < newMats.Length; i++)
+                    newMats[i] = overrideMaterial;
+
+                r.sharedMaterials = newMats;
+            }
         }
 
-        foreach (var kvp in _originalMaterials)
+        public static void RestoreOriginalMaterials()
         {
-            var r = kvp.Key;
-            if (r == null) continue;
+            if (!_hasStoredMaterials) return;
 
-            var newMats = new Material[r.sharedMaterials.Length];
-            for (var i = 0; i < newMats.Length; i++)
-                newMats[i] = overrideMaterial;
+            foreach (var kvp in _originalMaterials)
+            {
+                var r = kvp.Key;
+                var mats = kvp.Value;
 
-            r.sharedMaterials = newMats;
+                if (r != null)
+                    r.sharedMaterials = mats;
+            }
+
+            _originalMaterials.Clear();
+            _hasStoredMaterials = false;
         }
-    }
-
-    public static void RestoreOriginalMaterials()
-    {
-        if (!_hasStoredMaterials) return;
-
-        foreach (var kvp in _originalMaterials)
-        {
-            var r = kvp.Key;
-            var mats = kvp.Value;
-
-            if (r != null)
-                r.sharedMaterials = mats;
-        }
-
-        _originalMaterials.Clear();
-        _hasStoredMaterials = false;
     }
 }
