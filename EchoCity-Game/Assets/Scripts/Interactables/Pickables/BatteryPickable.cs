7a0cae93dace98c18e53ae0cc9143d4d266773b8
Samuel Runmark Thunell <samuel.runmark@gmail.com>
2026-01-10 16:05:03 +0100

    chore(pickables): clean up unneeded references

--- EchoCity-Game/Assets/Scripts/Interactables/Pickables/BatteryPickable.cs
+++ EchoCity-Game/Assets/Scripts/Interactables/Pickables/BatteryPickable.cs
@@ -4,17 +4,12 @@ namespace EchoCity
 {
     public class BatteryPickable : Pickable
     {
-        [Header("Invoking Events")]
-        [SerializeField] private SOShowUIEvent showUIEvent;
-        [Header("Messages")]
-        [SerializeField] private SODialogContainer dialogContainer;
         protected override void ResolveInteraction(bool outcome)
         {
             base.ResolveInteraction(outcome);
             if (outcome)
             {
                 showUIEvent?.RaiseEvent(this, ShowableUIEnum.PopUpMessage, new PopUpMessageParams("Battery Picked Up!", new Color(1f, 0.5f, 0f, 1f)));
-                // showUIEvent?.RaiseEvent(this, ShowableUIEnum.Narration, new NarrationParams(new DialogData(dialogContainer)));
             }
         }
     }
