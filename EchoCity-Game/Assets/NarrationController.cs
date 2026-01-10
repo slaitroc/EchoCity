843ba7f056628df1afc6f20e88b02d8517fea311
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2026-01-10 22:24:23 +0100

    fix(narration): comment out unused _isReady variable to hide warning

--- EchoCity-Game/Assets/NarrationController.cs
+++ EchoCity-Game/Assets/NarrationController.cs
@@ -23,7 +23,7 @@ namespace EchoCity
         private int _index;
         private int _cachedIndex;
         private bool _isClosed;
-        private bool _isReady;
+        // private bool _isReady;
         private SceneEnum _destinationScene;
         private bool _isFading;
         private int _queuedNext;
@@ -37,7 +37,7 @@ namespace EchoCity
 
             _index = 0;
             _isClosed = false;
-            _isReady = false;
+            // _isReady = false;
 
             StartCoroutine(InitCallbacksNextFrame());
         }
