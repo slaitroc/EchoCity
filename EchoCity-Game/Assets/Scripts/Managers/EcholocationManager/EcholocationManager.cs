1e2fab8eca4d1fb0b1f5e5aa23e2c541404dabb8
Samuel Runmark Thunell <samuel.runmark@gmail.com>
2026-01-18 01:52:58 +0100

    fix(refactor): refactor of shader switcher
    - Previous implementation of the shader switcher (MaterialSwitcher) was
    hastily put together to make URP Lit shading possible by switching to
    the EchoMaterial (with the AudioShader)
      - This implementation was flawed as it cached all materials in the
    scene to later reapply the materials during runtime
      - This introduces a ton of overhead due to storing all these materials
    and whenever the scene has non instantiated objects (such as the
    respawn)
    - This commit aims to refactor this into using a "fork" of the URP Lit
    shader and modify it directly to do the switching
      - The forwardspass of the URP shader now also has a contribution of
    the AudioShader which is set by a Unity shader keyword _Echolocation
    - The shader can be switched on and off with the old setMaterial event
    but now the EcholocationManager keeps track of the state of which shader
    is applied
    - This should also fix the issue of newly instansiated objects not
    having the shader

--- EchoCity-Game/Assets/Scripts/Managers/EcholocationManager/EcholocationManager.cs
+++ EchoCity-Game/Assets/Scripts/Managers/EcholocationManager/EcholocationManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace EchoCity
 {
@@ -12,8 +13,6 @@ namespace EchoCity
 
     public class EcholocationManager : MonoBehaviour, IEventSender
     {
-        private const int MAX_AUDIO_SPHERES = 64;
-
         [Header("Invoking Events")]
         [SerializeField] private SOEchoMaterialUpdated echoMaterialUpdatedEvent;
 
@@ -28,23 +27,16 @@ namespace EchoCity
 
 
         [Header("Echolocation Settings")]
-        [SerializeField] private bool useEcholocationMaterial = false;
-        [SerializeField] private Material echolocationMaterial;
-
-        [SerializeField] private VisualizationMode visualizationMode = VisualizationMode.GridLines;
-
         [Range(0.01f, 1.0f)][SerializeField] private float fadeInDuration = 0.1f;
 
         [Range(0.1f, 3.0f)][SerializeField] private float fadeOutDuration = 0.5f;
 
-        [Header("Grid Settings")]
-        [Range(0.1f, 5.0f)]
-        [SerializeField]
-        private float gridCellSize = 1.0f;
-
-        [Range(0.01f, 0.2f)][SerializeField] private float gridWidth = 0.05f;
-
-        [Range(0.01f, 0.3f)][SerializeField] private float gridPointSize = 0.08f;
+        private const int MAX_AUDIO_SPHERES = 64;
+        private bool _isEcholocationActive;
+        public bool IsEcholocationActive => _isEcholocationActive;
+        private const string GlobalToggleProp = "_Echolocation";
+        private const string KeywordOn = "ECHOLOCATION_ON";
+        private MaterialPropertyBlock _mpb;
 
         [SerializeField] private List<AudioSphere> activeSpheres = new();
         private readonly float[] sphereFrequencies = new float[MAX_AUDIO_SPHERES];
@@ -52,16 +44,12 @@ namespace EchoCity
 
         private readonly Vector4[] spherePositions = new Vector4[MAX_AUDIO_SPHERES];
         private readonly float[] sphereRadii = new float[MAX_AUDIO_SPHERES];
-        private int gridSizeID;
-        private int lineWidthID;
-        private int pointSizeID;
         private int sphereCountID;
         private int sphereFrequenciesID;
         private int sphereIntensitiesID;
 
         private int spherePositionsID;
         private int sphereRadiiID;
-        private int visualizationModeID;
 
         private void Awake()
         {
@@ -70,10 +58,11 @@ namespace EchoCity
             sphereFrequenciesID = Shader.PropertyToID("_AudioSphereFrequencies");
             sphereIntensitiesID = Shader.PropertyToID("_AudioSphereIntensities");
             sphereCountID = Shader.PropertyToID("_AudioSphereCount");
-            gridSizeID = Shader.PropertyToID("_GridSize");
-            lineWidthID = Shader.PropertyToID("_LineWidth");
-            pointSizeID = Shader.PropertyToID("_PointSize");
-            visualizationModeID = Shader.PropertyToID("_VisualizationMode");
+
+            _mpb = new MaterialPropertyBlock();
+            _isEcholocationActive = false;
+            Shader.SetGlobalInt(GlobalToggleProp, 0);
+            Shader.DisableKeyword(KeywordOn);
         }
 
         void OnEnable()
@@ -89,22 +78,22 @@ namespace EchoCity
             if (setMaterialEvent) setMaterialEvent.OnEventRaised -= SetMaterialHandler;
         }
 
-        private void OnValidate()
-        {
-            if (Application.isPlaying)
-            {
-                if (useEcholocationMaterial && echolocationMaterial != null)
-                {
-                    MaterialSwitcher.ApplyOverrideMaterial(echolocationMaterial);
-                    Log.DLazy(() => "Echolocation Material Applied", this);
-                }
-                else
-                {
-                    MaterialSwitcher.RestoreOriginalMaterials();
-                    Log.DLazy(() => "Restored Original Materials", this);
-                }
-            }
-        }
+        // private void OnValidate()
+        // {
+        //     if (Application.isPlaying)
+        //     {
+        //         if (_isEcholocationActive) // TODO: ????
+        //         {
+        //             SetEcholocationActive(true);
+        //             Log.DLazy(() => "Echolocation Material Applied", this);
+        //         }
+        //         else
+        //         {
+        //             SetEcholocationActive(false);
+        //             Log.DLazy(() => "Restored Original Materials", this);
+        //         }
+        //     }
+        // }
 
         private void Update()
         {
@@ -146,10 +135,6 @@ namespace EchoCity
             Shader.SetGlobalFloatArray(sphereFrequenciesID, sphereFrequencies);
             Shader.SetGlobalFloatArray(sphereIntensitiesID, sphereIntensities);
             Shader.SetGlobalInt(sphereCountID, count);
-            Shader.SetGlobalFloat(gridSizeID, gridCellSize);
-            Shader.SetGlobalFloat(lineWidthID, gridWidth);
-            Shader.SetGlobalInt(visualizationModeID, (int)visualizationMode);
-            Shader.SetGlobalFloat(pointSizeID, gridPointSize);
         }
 
 
@@ -183,70 +168,68 @@ namespace EchoCity
             activeSpheres.Add(newSphere);
         }
 
-        public void ToggleMaterialHandler(IEventSender sender)
-        {
-            useEcholocationMaterial = !useEcholocationMaterial;
-
-            if (useEcholocationMaterial && echolocationMaterial != null)
-            {
-                MaterialSwitcher.ApplyOverrideMaterial(echolocationMaterial);
-                Log.DLazy(() => "Toggle: Switched to Echolocation Material", this);
-                echoMaterialUpdatedEvent?.RaiseEvent(this, true);
-            }
-            else
-            {
-                MaterialSwitcher.RestoreOriginalMaterials();
-                Log.DLazy(() => "Toggle: Restored Original Materials", this);
-                echoMaterialUpdatedEvent?.RaiseEvent(this, false);
-            }
-
-        }
 
         public void SetMaterialHandler(IEventSender sender, EchoMaterialCodeEnum code)
         {
             switch (code)
             {
                 case EchoMaterialCodeEnum.Active:
-                    // if (useEcholocationMaterial == true) return;
-                    if (echolocationMaterial)
-                    {
-                        MaterialSwitcher.ApplyOverrideMaterial(echolocationMaterial);
-                        Log.DLazy(() => "Switched to Echolocation Material", this);
-                        useEcholocationMaterial = true;
-                        echoMaterialUpdatedEvent?.RaiseEvent(this, true);
-                    }
+                    SetEcholocationActive(true);
+                    Log.DLazy(() => "Switched to Echolocation Shader", this);
+                    echoMaterialUpdatedEvent?.RaiseEvent(this, true);
                     break;
                 case EchoMaterialCodeEnum.Inactive:
-                    // if (useEcholocationMaterial == false) return;
-                    if (echolocationMaterial)
-                    {
-                        MaterialSwitcher.RestoreOriginalMaterials();
-                        Log.DLazy(() => "Restored Original Materials", this);
-                        useEcholocationMaterial = false;
-                        echoMaterialUpdatedEvent?.RaiseEvent(this, false);
-                    }
+                    SetEcholocationActive(false);
+                    Log.DLazy(() => "Restored Original Shader", this);
+                    echoMaterialUpdatedEvent?.RaiseEvent(this, false);
                     break;
                 case EchoMaterialCodeEnum.ReApply:
-                    if (useEcholocationMaterial && echolocationMaterial != null)
-                    {
-                        MaterialSwitcher.RestoreOriginalMaterials(); //FIXME
-                        MaterialSwitcher.ApplyOverrideMaterial(echolocationMaterial);
-                        Log.DLazy(() => "Reapplied Echolocation Material", this);
-                    }
-
-                    if (!useEcholocationMaterial)
-                    {
-                        MaterialSwitcher.RestoreOriginalMaterials();
-                        Log.DLazy(() => "Restored Original Materials", this);
-                    }
+                    SetEcholocationActive(false);
+                    SetEcholocationActive(true);
+                    Log.DLazy(() => "Reapplied Echolocation Shader", this);
                     break;
                 case EchoMaterialCodeEnum.Toggle:
-                    ToggleMaterialHandler(sender);
+                    if (_isEcholocationActive)
+                    {
+                        SetEcholocationActive(false);
+                        Log.DLazy(() => "Toggle: Set URP Lit Shader", this);
+                        echoMaterialUpdatedEvent?.RaiseEvent(this, false);
+                    }
+                    else
+                    {
+                        SetEcholocationActive(true);
+                        Log.DLazy(() => "Toggle: Set AudioShader", this);
+                        echoMaterialUpdatedEvent?.RaiseEvent(this, true);
+                    }
                     break;
                 default:
                     Log.ELazy(() => "SetMaterialHandler received wrong code", this);
                     break;
             }
         }
+
+        public void SetEcholocationActive(bool active)
+        {
+            if (_isEcholocationActive == active) return;
+            _isEcholocationActive = active;
+
+            Shader.SetGlobalInt(GlobalToggleProp, active ? 1 : 0);
+            if (active)
+                Shader.EnableKeyword(KeywordOn);
+            else
+                Shader.DisableKeyword(KeywordOn);
+
+            foreach (var r in FindObjectsByType<Renderer>(
+                FindObjectsInactive.Include,
+                FindObjectsSortMode.None
+            ))
+            {
+                r.GetPropertyBlock(_mpb);
+
+                _mpb.SetFloat(GlobalToggleProp, active ? 1f : 0f);
+                r.SetPropertyBlock(_mpb);
+            }
+        }
+
     }
 }
---
+++ EchoCity-Game/Assets/Scripts/Utility/FindMissingEcholocationShader.cs
@@ -0,0 +1,116 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EchoCity.EditorTools
+{
+    public class FindMissingEcholocationShader : EditorWindow
+    {
+        private class RendererInfo
+        {
+            public GameObject GameObject;
+            public string GameObjectName;
+            public Material Material;
+            public string MaterialName;
+            public string CurrentShader;
+            public Renderer Renderer;
+        }
+
+        private Vector2 scrollPosition;
+        private List<RendererInfo> cache = new List<RendererInfo>();
+        private string targetShaderName = "Custom/EcholocationLit";
+        private string sourceShaderName = "Universal Render Pipeline/Lit";
+
+        [MenuItem("Tools/EchoCity/Find Objects Without Echolocation Shader")]
+        public static void ShowWindow()
+        {
+            GetWindow<FindMissingEcholocationShader>("Replace URP Lit Shader");
+        }
+
+        private void OnGUI()
+        {
+            sourceShaderName = EditorGUILayout.TextField("Source Shader:", sourceShaderName);
+            targetShaderName = EditorGUILayout.TextField("Target Shader:", targetShaderName);
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("Search Loaded Scenes", GUILayout.Height(30)))
+            {
+                cache.Clear();
+
+                for (int i = 0; i < SceneManager.sceneCount; i++)
+                {
+                    Scene scene = SceneManager.GetSceneAt(i);
+
+                    GameObject[] rootObjects = scene.GetRootGameObjects();
+                    foreach (GameObject root in rootObjects)
+                    {
+                        Renderer[] renderers = root.GetComponentsInChildren<Renderer>(true);
+                        foreach (Renderer renderer in renderers)
+                        {
+                            foreach (Material mat in renderer.sharedMaterials)
+                            {
+                                if (mat == null) continue;
+
+                                if (mat.shader == Shader.Find(sourceShaderName))
+                                {
+                                    cache.Add(new RendererInfo
+                                    {
+                                        GameObject = renderer.gameObject,
+                                        GameObjectName = renderer.gameObject.name,
+                                        Material = mat,
+                                        MaterialName = mat.name,
+                                        CurrentShader = mat.shader.name,
+                                        Renderer = renderer
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            EditorGUILayout.Space();
+
+            if (cache.Count > 0)
+            {
+                GUILayout.Label($"Found {cache.Count} objects without the shader");
+
+                scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+                foreach (var info in cache)
+                {
+                    EditorGUILayout.LabelField(info.GameObjectName);
+                }
+                EditorGUILayout.EndScrollView();
+
+                EditorGUILayout.Space();
+
+                if (GUILayout.Button("Fix All", GUILayout.Height(25)))
+                {
+                    if (!EditorUtility.DisplayDialog("Fix All Shaders",
+                        $"This will change {cache.Count} materials to use '{targetShaderName}'. Continue?",
+                        "Yes", "Cancel"))
+                    {
+                        return;
+                    }
+
+                    foreach (var info in cache.ToList())
+                    {
+                        Undo.RecordObject(info.Material, "Change Shader");
+                        info.Material.shader = Shader.Find(targetShaderName);
+                        EditorUtility.SetDirty(info.Material);
+                    }
+
+                    cache.Clear();
+                }
+            }
+            else if (cache.Count == 0 && GUILayout.Button("Clear Results"))
+            {
+                cache.Clear();
+            }
+        }
+
+    }
+}
