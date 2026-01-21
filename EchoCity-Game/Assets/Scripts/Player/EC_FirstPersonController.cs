53074e51f044b5bbeb2607a95a3c63dfa2b342cb
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2026-01-21 13:37:05 +0100

    fix(controller): ensure player movement only occurs when controller is enabled

--- EchoCity-Game/Assets/Scripts/Player/EC_FirstPersonController.cs
+++ EchoCity-Game/Assets/Scripts/Player/EC_FirstPersonController.cs
@@ -301,8 +301,9 @@ namespace StarterAssets
                 inputDirection = transform.right * _input.move.x + transform.forward * _input.move.y;
             }
 
-            // move the player
-            _controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+            if (_controller.enabled == true)
+                // move the player
+                _controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
 
             // Determina lo stato di movimento corrente - priorità: Move/Sprint > Look > Idle
             if (_input.move != Vector2.zero)
